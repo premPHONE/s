e74da0152b9eb8ada9ab829fbf8b2262cd0815b3
premPHONE <107094344+premPHONE@users.noreply.github.com>
2022-06-08 16:10:43 +0530

    Add files via upload

---
+++ cir/class1.php
@@ -0,0 +1,119 @@
+<?php include "header.php" ;?>
+                <!--End of Header Area-->
+                <!--Breadcrumb Banner Area Start-->
+                <div class="breadcrumb-banner-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="breadcrumb-text">
+                                    <h1 class="text-center">LKG</h1>
+                                    <div class="breadcrumb-bar">
+                                        <ul class="breadcrumb text-center">
+                                            <li><a href="index_sample.php">Home</a></li>
+                                            <li>LKG</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--End of Breadcrumb Banner Area-->
+                <!--Event Area Start-->
+                <div style="padding: 1px;height: 30px; background-color: lightgreen;"><center><h6><b>A.1 Skipping- counting</b></h6></center></div>
+                    <div class="container">
+                       
+                        <div class="row">
+                            <div class="col-lg-4 col-md-6 col-12">
+                                <div class="single-event-item">
+                                    <div class="single-event-image">
+                                        <a href="#">
+                                            <!-- <img src="img/event/1.jpg" alt=""> -->
+                                            <!-- <span><span>15</span>Jun</span> -->
+                                        </a>
+                                    </div>
+
+                                    
+                                </div>
+                            </div>
+                                       <div class="col-lg-4 col-md-6 col-12">
+                                <div class="single-event-item">
+                                    
+                                   
+                                </div>
+                            </div>
+                                       <div class="col-lg-4 col-md-6 col-12">
+                                <div class="single-event-item">
+                                    
+                                    
+                                    
+                                </div>
+                            </div>
+                                       <div class="col-lg-4 col-md-6 col-12">
+                                <div class="single-event-item">
+                                    <div class="single-event-image">
+                                        <a href="#">
+                                            <!-- <img src="img/event/1.jpg" alt=""> -->
+                                            <!-- <span><span>15</span>Jun</span> -->
+                                        </a>
+                                    </div>
+                                    
+                                    <div class="">
+                                        <center><h5><a href="nameshape.php">Questions</a></h5><span>0</span></center>
+                                        <!-- <div class="single-item-comment-view">
+                                           <span>0</span>
+                                           <span><i class="zmdi zmdi-pin"></i>Dhaka Bangladesh</span>
+                                       </div> -->
+                                      <!--  <p>There are many variaons of passa of Lorem Ipsuable, amrn in sofby injected humour, amr sarata din megla....</p> -->
+                                       <!-- <a class="button-default" href="nameshape.php">0</a> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-12">
+                                <div class="single-event-item">
+                                    <div class="single-event-image">
+                                        <a href="#">
+                                           <!--  <img src="img/event/2.jpg" alt=""> -->
+                                           <!--  <span><span>20</span>Apr</span> -->
+                                        </a>
+                                    </div>
+                                    <div class="">
+                                       <center><h5><a href="#">Time</a></h5><span>6.30</span></center> 
+                                        <!-- <div class="single-item-comment-view">
+                                           <span>6.30</span>
+                                           <span><i class="zmdi zmdi-pin"></i>Jessore Bangladesh</span>
+                                       </div> -->
+                                      <!--  <p>There are many variaons of passa of Lorem Ipsuable, amrn in sofby injected humour, amr sarata din megla....</p> -->
+                                      <!--  <a class="button-default" href="#">6.30</a> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-12">
+                                <div class="single-event-item">
+                                    
+                                    
+                                        <center><h5><a href="#">Smart Score</a></h5> <span>0</span></center>
+                                        <!-- <div class="single-item-comment-view">
+                                           <span>0</span>
+                                           <span><i class="zmdi zmdi-pin"></i>Dhaka. Bangladesh</span>
+                                       </div> -->
+                                      <!--  <p>There are many variaons of passa of Lorem Ipsuable, amrn in sofby injected humour, amr sarata din megla....</p> -->
+                                       <!-- <a class="button-default" href="#">0</a> -->
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br><br><br>
+<div class="container">
+                    <h4>1.Count the fruits</h4>
+                <br><br><br><br><img src="img/cirimg/cir1.png">
+<br>
+<input type="text" name="">
+<br><br>
+                <button class="btn button-default">Submit</button>
+                </div><br><br>
+                <br><br>
+                <!--End of Event Area-->
+                      <!--End of Newsletter Area-->
+<?php include "footer.php" ;?>
